02b4a808ef9f5e73dabec79576e1ccd99867ecf5
nikhilkrathi <nikhilkrathi@gmail.com>
2016-11-01 18:25:55 +0530

    Update SpaceAce.c

--- SpaceAce.c
+++ SpaceAce.c
@@ -1,9 +1,9 @@
-/*	Copyright (C) 2016 Nikhil Rathi  
+/*  Copyright (C) 2016 Nikhil Rathi  
     This file is part of SpaceAce.
 
     SpaceAce is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+    Nikhil Rathi, either version 3 of the License, or
     (at your option) any later version.
 
     SpaceAce is distributed in the hope that it will be useful,
@@ -11,8 +11,8 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with SpaceAce.  If not, see <http://www.gnu.org/licenses/>
+    For a copy of the GNU General Public License ,
+    see <http://www.gnu.org/licenses/>
 */
 
 #include <stdio.h>
