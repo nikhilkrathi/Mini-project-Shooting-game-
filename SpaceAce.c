ea37a08fd766d39d8cdd41df84a4129430ce3d49
nikhilkrathi <nikhilkrathi@gmail.com>
2016-11-02 19:45:36 +0530

    Update SpaceAce.c

--- SpaceAce.c
+++ SpaceAce.c
@@ -1,20 +1,3 @@
-/*  Copyright (C) 2016 Nikhil Rathi  
-    This file is part of SpaceAce.
-
-    SpaceAce is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    Nikhil Rathi, either version 3 of the License, or
-    (at your option) any later version.
-
-    SpaceAce is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    For a copy of the GNU General Public License ,
-    see <http://www.gnu.org/licenses/>
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,46 +8,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include "SpaceAce.h"
 
 #define MAX_BOMBS 100000
-
-
-struct player {
-	int r,c;
-	char ch;
-};
-
-struct alien {
-	int r,c;
-	int pr,pc;
-	int alive; /* 1=alive 0=destroyed */
-	char direction; /* 'u'= up 'd'= down */
-	char ch;
-};
-
-struct shoot {
-	int r,c;
-	int active; /* 1=active 0=inactive */
-	char ch;
-};
-
-struct bomb {
-	int r,c;
-	int active; /* 1=active 0=inactive */
-	int loop;
-	char ch;
-};
-
-struct options {
-	int overall,alien,shots,bombs,bombchance;
-	double speed;
-};
-
-
-struct entry {
-   	char name[128];
-   	int score;
-};
 
 void gameover(int win, int score);
 void highscores();
